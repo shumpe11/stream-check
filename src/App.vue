745906607f4266cd5dd0abb6f6863641af77de9b
shumpe11 <ymok3apei@gmail.com>
2020-11-28 23:20:29 +0900

    サイドバーの追加

--- src/App.vue
+++ src/App.vue
@@ -5,56 +5,36 @@
       color="primary"
       dark
     >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
+      <v-app-bar-nav-icon @click="openSideNavBar"></v-app-bar-nav-icon>
+      <v-toolbar-title>Stream Check</v-toolbar-title>
       <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
     </v-app-bar>
 
     <v-main>
-      <HelloWorld/>
+      <SideNavbar :drawer="this.drawer"></SideNavbar>
     </v-main>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+import SideNavbar from './components/SideNavBar'
 
 export default {
-  name: 'App',
 
-  components: {
-    HelloWorld,
-  },
+name: 'App',
 
-  data: () => ({
-    //
-  }),
+components: {
+  SideNavbar
+},
+
+data: () => ({
+  drawer:false
+}),
+
+methods: {
+  openSideNavBar () {
+    this.drawer = !this.drawer
+  }
+}
 };
 </script>
---
+++ src/components/SideNavBar.vue
@@ -0,0 +1,45 @@
+<template>
+    <v-container fluid fill-height>
+
+        <v-navigation-drawer v-model="drawer" absolute temporary>
+            <v-list>
+                <v-list-item>
+                    <v-list-item-avatar>
+                        <img src="https://avatars2.githubusercontent.com/u/1363954?s=460&v=4">
+                    </v-list-item-avatar>
+                    <v-list-item-content>
+                        <v-list-item-title>Kazuya Kojima</v-list-item-title>
+                    </v-list-item-content>
+                </v-list-item>
+
+        <v-divider></v-divider>
+
+                <v-list-item v-for="(item, index) in items" :key="index">
+                    <v-list-item-icon>
+                        <v-icon>{{ item.icon }}</v-icon>
+                    </v-list-item-icon>
+                    <v-list-item-content>
+                        <v-list-item-title>{{ item.title }}</v-list-item-title>
+                    </v-list-item-content>
+                </v-list-item>
+            </v-list>
+        </v-navigation-drawer>
+    </v-container>
+</template>
+
+<script>
+export default {
+
+props:['drawer'],
+
+data () {
+    return {
+        items: [
+        { title: 'お気に入りストリーマー一覧', icon: 'mdi-menu' }
+        ]
+    }
+},
+
+
+}
+</script>
