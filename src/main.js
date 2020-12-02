88ccdb9789df3572ccc5f89a7f07c7802f72aca1
shumpe11 <ymok3apei@gmail.com>
2020-12-02 23:33:51 +0900

    firebaseのAPIkeyを環境変数を参照するように

--- src/main.js
+++ src/main.js
@@ -7,6 +7,17 @@ import vuetify from './plugins/vuetify'
 
 Vue.config.productionTip = false
 
+var firebaseConfig = {
+  apiKey: process.env.VUE_APP_FIREBASE_API_KEY,
+  authDomain: "streamcheck-296608.firebaseapp.com",
+  databaseURL: "https://streamcheck-296608.firebaseio.com",
+  projectId: "streamcheck-296608",
+  storageBucket: "streamcheck-296608.appspot.com",
+  messagingSenderId: "454068489063",
+  appId: "1:454068489063:web:a6f51ff49435eee64c7dc2",
+  measurementId: "G-XJ5RVWPJMW"
+};
+
 new Vue({
   router,
   store,
